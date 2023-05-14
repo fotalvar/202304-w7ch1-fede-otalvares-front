fa24767a9ecc173e7c039ecb79a7d4cb7b232d88
Fede Otálvares <fnotalvares@gmail.com>
2023-05-14 15:55:48 +0200

    Create robotsData mock for testing

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,12 @@
+import RobotStructure from "../store/robotsSlice/types";
+
+export const robotsData: RobotStructure[] = [
+  {
+    _id: "645f591ba5e9f42ac8b63e55",
+    name: "Bender",
+    image: "https://i.ibb.co/H4n9bkM/bender.png",
+    speed: 7,
+    resilience: 9,
+    creationDate: " 2996",
+  },
+];
