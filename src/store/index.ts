7e795e5d1acfcca4ced424cefb861598f554bf9a
nary lozano <narylozano@gmail.com>
2023-05-14 08:23:08 +0200

    Refactor list and app to use types

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,6 +14,7 @@ export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
     preloadedState,
   });
 };
+
 export const store = setupStore();
 
 export type RootState = ReturnType<typeof rootReducer>;
